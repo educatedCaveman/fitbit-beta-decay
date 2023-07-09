8f394e54fb58100682e538cad31a5b41cbc8856c
John Hoffman <j.drake.hoffman@gmail.com>
2023-07-09 13:03:21 -0400

    COD-21
    duplicate for other versions

---
+++ beta-decay-free/app/clock.js
@@ -0,0 +1,45 @@
+import clock from "clock";
+import { battery } from "power";
+// import * as complication from "./complications";
+import * as steps from "./steps";
+// import * as simpleSettings from "./device-settings";
+
+let clockCallback;
+let glitchStr = "";
+const allChars = "\"!#$%&'()*+,-./1234567890:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~¡¢£¥¦¨©«®°±²³´¶¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ‐–—‘’“”…█"
+
+export function initialize(granularity, callback) {
+    clock.granularity = granularity;
+    clockCallback = callback;
+    clock.addEventListener("tick", tickHandler);
+}
+
+
+function generateGlitchTxt() {
+
+    if (glitchStr != "") {
+        //scrolling 
+        let index = Math.floor(Math.random() * allChars.length);
+        glitchStr = String(glitchStr + allChars[index]).slice(-5);
+    } else {
+        // initialize, or refresh whole glitch
+        glitchStr = "";
+        for (let i = 0; i < 5; i++) {
+            let index = Math.floor(Math.random() * allChars.length);
+            glitchStr = glitchStr + allChars[index];
+        }
+    }
+    return glitchStr;
+}
+
+
+function tickHandler(evt) {
+    // callback
+    clockCallback({
+        steps: steps.getFormattedSteps(),
+        batt: String(battery.chargeLevel + "%"),
+        comp: generateGlitchTxt(),
+    });
+
+}
+
---
+++ beta-decay-free/app/index.js
@@ -0,0 +1,94 @@
+import document from "document";
+import * as drawText from "../../common/app/draw-text.js"
+import * as nonClock from "../../common/app/clock.js"
+import * as heartRateMon from "../../common/app/hrm.js"
+// import * as simpleSettings from "../../common/app/device-settings.js"
+
+
+/* --------- Document/UI elements ---------- */
+let background = document.getElementById("background");
+let textBG = document.getElementById("text_bg");
+let text = document.getElementById("foreground");
+let label = document.getElementById("label");
+
+
+/* --------- DRAW BACKGROUND ---------- */
+drawText.drawBackground();
+
+
+/* --------- CLOCK ---------- */
+// steps, battery level, and extra thingy
+function clockCallback(data) {
+    //steps
+    drawText.drawBigText(data.steps, 'step_fg');
+
+    //batt
+    drawText.drawLittleText(data.batt, 'batt_fg');
+
+    //complication
+    drawText.drawLittleText(data.comp, 'comp_fg');
+}
+// seconds for testing. minutes for IRL
+nonClock.initialize("seconds", clockCallback);
+
+
+/* --------- HRM ---------- */
+function hrmCallback(data) {
+    //heart rate
+    drawText.drawBigText(data.bpm, 'heart_fg')
+}
+heartRateMon.initialize(hrmCallback);
+
+
+// /* -------- SETTINGS -------- */
+// function settingsCallback(data) {
+//     // handle no data
+//     if (!data) {
+//         return;
+//     }
+
+//     // text color
+//     if (data.colorText) {
+//         text.style.fill = data.colorText;
+//     }
+
+//     // text background color. preset
+//     if (data.colorTextBackground) {
+//         textBG.style.fill = data.colorTextBackground;
+//     }
+
+//     // text background opacity
+//     if (data.opacityTextBackground) {
+//         textBG.style.fillOpacity = data.opacityTextBackground / 100;
+//     }
+
+//     // background color
+//     if (data.colorBackground) {
+//         background.style.fill = data.colorBackground;
+//     }
+
+//     // label color
+//     if (data.colorLabel) {
+//         label.style.fill = data.colorLabel;
+//     }
+
+//     // override/custom colors
+//     // text color
+//     if (data.customText && simpleSettings.getSettingsVal('customizeText')) {
+//         text.style.fill = data.customText.name;
+//     }
+//     // text background color
+//     if (data.customTextBackground && simpleSettings.getSettingsVal('customizeTextBackground')) {
+//         textBG.style.fill = data.customTextBackground.name;
+//     }
+//     // background color
+//     if (data.customBackground && simpleSettings.getSettingsVal('customizeBackground')) {
+//         background.style.fill = data.customBackground.name;
+//     }
+//     // label color
+//     if (data.customLabel && simpleSettings.getSettingsVal('customizeLabel')) {
+//         label.style.fill = data.customLabel.name;
+//     }
+
+// }
+// simpleSettings.initialize(settingsCallback);
